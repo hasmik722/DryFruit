b5bf6f5391abb8a05597dd1550477f0d26b5e984
Hasmik Margaryan <hasmikmargaryan277@gmail.com>
2022-03-12 23:49:55 +0400

    Added readMore part

--- js/script.js
+++ js/script.js
@@ -1,17 +1,59 @@
 let navbar = document.querySelector('.navbar');
-// let searchForm = document.querySelector('.search-form');
+// let dots = document.getElementById("dots");
+// let moreText = document.getElementById("more");
+// let btnText = document.querySelector('.btn')
+let dots = document.getElementById("dots");
+let moreText = document.getElementById("more");
+let btnText = document.getElementById("btn");
+
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
-    // searchForm.classList.remove('active');
 }
-
-// document.querySelector('#search-btn').onclick = () => {
-//     searchForm.classList.toggle('active');
-//     navbar.classList.remove('active');
-// }
 
 window.onscroll = () => {
     navbar.classList.remove('active');
-    // searchForm.classList.remove('active');
 }
+
+
+function myFunction() {
+    
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myFunctionArm() {
+    
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Կարդալ Ավելին"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Փակել"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myFunctionRu() {
+    
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Узнать больше"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Закрыть"; 
+      moreText.style.display = "inline";
+    }
+  }
